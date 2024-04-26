d0bdc80becedfc32dbd281c2548d472451c52396
Lenovo131\User4_13_1 <mirco.castino.13-1@accademiadigitaleliguria.it>
2024-04-26 22:43:52 +0200

    Creata funzione di stampa del menu del ristorante

---
+++ Codice/lib/funzione_Menu_Ristorante.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+void Menu_Ristorante();
+
+int main()
+{
+    Menu_Ristorante();
+
+    return 0;
+}
+void Menu_Ristorante()
+{
+    printf("TAJ MAHAL - INDIAN CUISINE\n");
+    printf("ORARI:\t12.00 - 03.00 / 18.30 - 22-30\n\n");
+
+    printf("ANTIPASTI\n");
+    printf("- VEGETABLE SAMOSA € 5.00\n  - Paste triangolari ripiene di verdure\n");
+    printf("- ONION BAJI € 5.00\n  - Frittelle di cipolla con farina di ceci\n");
+    printf("- VEGETABLE PAKORA € 5.00\n  - Frittelle miste di verdure con farina di ceci\n");
+    printf("- VEGETABLE CUTLET € 5.00\n  - Polpettine di verdure miste\n");
+    printf("- PANEER TIKKA € 6.00\n  - Tipico formaggio Indiano arrostito\n");
+    printf("- CHICKEN TIKKA € 7.00\n  - Spiedini di pollo marinati con yogurt e spezie\n");
+    printf("- PRAWN PAKORA € 8.00\n  - Frittelle di gamberi con farina di ceci\n");
+    printf("- BOMBAY MIX € 7.00\n  - Assaggio misto delle nostre specialità\n\n");
+
+    printf("NAAN (PANE) € 1.50\n");
+    printf("- PLAN NAAN : pane al naturale\n");
+    printf("- CHEESE NAAN : pane al formaggio\n");
+    printf("- ALOO NAAN : pane alle patate\n");
+    printf("- ONION NAAN : pane alle cipolle\n\n");
+
+    printf("BIRYANI (RISO BASMATI)\n");
+    printf("- DRY FRUIT RICE € 6.00\n  - Riso basmati con frutta secca\n");
+    printf("- CHICKEN BIRYANI € 7.00\n  - Riso basmati con bocconcini di pollo e spezie\n");
+    printf("- VEGETABLE BIRYANI € 6.00\n  - Riso basmati con verdure e spezie\n");
+    printf("- MIX BIRYANI € 7.00\n  - Riso basmati con misto di carni e verdure\n\n");
+
+    printf("VERDURE € 6.00\n");
+    printf("- DAL MASOOR\n  - Lenticchie rosse con erbette e spezie\n");
+    printf("- CHANA MASALA\n  - Ceci in umido con spezie\n");
+    printf("- BEGEN BARTHA\n  - Melanzane arrostite\n");
+    printf("- VEGETABLE KORMA\n  - Verdure immerse in una salsa di frutta secca\n\n");
+
+    printf("- SPECIALITÀ TANDOOR € 10.00\n");
+    printf("- MIX TANDOOR\n  - Grigliata mista di carne e pesce marinata con yogurt e spezie\n");
+    printf("- TANDOOR CHICKEN\n  - Coscette di pollo marinate con yogurt e arrostite al tandoor\n");
+    printf("- CHICKEN TIKKA\n  - Bocconcini di pollo marinati con yogurt e arrostiti al tandoor\n");
+    printf("- PORK TIKKA\n  - Spiedini di maiale arrostiti\n\n");
+
+    printf("- GAMBERI € 10.00\n");
+    printf("  - MALAI PRAWN : Gamberi con latte di cocco\n");
+    printf("  - PRAWN MASALA : Gamberi piccanti saltati in padella\n");
+    printf("  - PRAWN MADRAS : Gamberi al curry piccante\n");
+    printf("  - FISH MADRAS : Bocconcini di pesce al curry piccante\n\n");
+
+    printf("POLLO € 10.00\n");
+    printf("- KORMA\n  - Bocconcini di pollo con salsa alla frutta secca\n");
+    printf("- BUTTER\n  - Bocconcini di pollo con salsa al burro\n");
+    printf("- TIKKA MASALA\n  - Bocconcini di pollo al tandoor saltati con verdure e spezie\n");
+    printf("- MADRAS\n  - Pollo al curry piccante di Madras\n");
+    printf("- VINDALOO\n  - Pollo con patate speziate piccanti\n\n");
+
+    printf("AGNELLO € 10.00\n");
+    printf("- MADRAS\n  - Agnello al curry piccante con Madras\n");
+    printf("- KORMA\n  - Agnello con frutta secca\n");
+    printf("- VINDALOO\n  - Agnello con patate speziate\n\n");
+
+    printf("DOLCI € 3.00\nIl presente menu sostituisce i precedenti ed è valido da Aprile 2020\n\n");
+    printf("NB: In assenza di prodotti freschi potranna essere utilizzati prodotti surgelati\n\n");
+
+    printf("CESTINI MISTI\n\nVEGETARIANO € 10.00\nMISTO € 11.00\nGRIGLIATA TANDOOR € 11.00\n");
+}
