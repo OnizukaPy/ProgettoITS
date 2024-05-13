3af9a9afdf1d0413db99aadbfc0fe0f7a049fd16
OnizukaPy <perito@ivancatalano.eu>
2024-05-13 12:09:12 +0200

    Aggiunta la funzione di prenotazione del tavolo e migliorati alcuni pezzi di codice sulla gestione del logout utenti da parte del server

--- Codice/lib/funzioni.c
+++ Codice/lib/funzioni.c
@@ -69,15 +69,44 @@ int conta_righe(char *path){
         return 0;
     }
     // salviamo le righe del file
-    char riga[100];
+    char riga[MAX_LUNG_PORTATA];
     int i = 0;
     // leggiamo il file e salviamo le righe in un array di stringhe
-    while(fgets(riga, 100, file) != NULL){
+    while(fgets(riga, MAX_PORTATE, file) != NULL){
         i++;
     }
 
     return i;
 }
+
+// conversione data da gg/mm/aaaa in gg_mm_aaaa
+char* conversione_data(char *data){
+    char static data_conv[50];
+    strcpy(data_conv, data);
+    for (int i = 0; i < strlen(data_conv); i++){
+        if (data_conv[i] == '/'){
+            data_conv[i] = '_';
+        }
+    }
+    return data_conv;
+}
+
+bool se_esiste(char *path, char *nome_file, char* tipo_file){
+    char save_path[50];
+    sprintf(save_path, "%s/%s.%s", path, nome_file, tipo_file);
+    //printf("%s\n", save_path);
+    FILE *file = fopen(save_path, "r");
+    if(file == NULL){
+        //printf("il file non c'e'\n");
+        fclose(file);
+        return false;
+    } else {
+        //printf("il file c'e'\n");
+        fclose(file);
+        return true;
+    }
+}
+
 
 
 // Gestione password, cifratura e decifratura
@@ -452,6 +481,20 @@ void visualizza_sala(char *data){
         int n_tavoli = cJSON_GetArraySize(cJSON_GetObjectItem(sala, "tavoli"));
         printf("Il numero di tavoli e': %d\n", n_tavoli);
 
+        // elenchiamo i tavoli che sono liberi con i posti liberi e occupati
+        for (int i = 0; i < n_tavoli; i++){
+            cJSON *tavolo = cJSON_GetArrayItem(cJSON_GetObjectItem(sala, "tavoli"), i);
+            // printiamo se il tavolo è coccupato o meno
+            if (cJSON_IsTrue(cJSON_GetObjectItem(tavolo, "occupato"))){
+                printf("Il tavolo %d e' occupato\n", i + 1);
+            } else {
+                printf("Il tavolo %d e' libero\n", i + 1);
+                // printiamo i posti liberi e occupati
+                printf("Posti liberi: %d", cJSON_GetObjectItem(tavolo, "posti_liberi")->valueint);
+                printf(" Posti occupati: %d\n", cJSON_GetObjectItem(tavolo, "posti_occupati")->valueint);
+            }
+        }
+
         // calcoliamo quanti posti liberi totali ci sono nella sala
         int posti_liberi_totali = 0;
         for (int i = 0; i < n_tavoli; i++){
@@ -629,7 +672,7 @@ Portata *carica_menu(char *path){
     char *righe[MAX_PORTATE];
     int i = 0;
     // leggiamo il file e salviamo le righe in un array di stringhe
-    while(fgets(riga, 100, file) != NULL){
+    while(fgets(riga, MAX_PORTATE, file) != NULL){
         righe[i] = strdup(riga);
         i++;
     }
@@ -673,7 +716,124 @@ void visualizza_menu(char* path){
     }
 }
 
+// funzione per prenotare un tavolo
+void prenota_tavolo(char *username, char *data, cJSON* sala, char *path_temp){
+    
+    // facciamo un ciclo per chiedere che tavoli vuole prenotare e per quanti posti
+    int n_tavoli = cJSON_GetArraySize(cJSON_GetObjectItem(sala, "tavoli"));
+
+    // contiamo quanti tavoli sono liberi
+    int tavoli_liberi = 0;
+    for (int i = 0; i < n_tavoli; i++){
+        cJSON *tavolo = cJSON_GetArrayItem(cJSON_GetObjectItem(sala, "tavoli"), i);
+        if(cJSON_IsFalse(cJSON_GetObjectItem(tavolo, "occupato"))){
+            tavoli_liberi++;
+        }
+    }
+
+    printf("Ci sono %d tavoli liberi\n", tavoli_liberi);
+    /* int *tavoli_prenotati = malloc(n_tavoli * sizeof(int));
+    int *posti_prenotati = malloc(n_tavoli * sizeof(int)); */
+
+    /* // creiamo un array json per salvare i tavoli prenotati e i posti prenotati
+    cJSON *tavoli_prenotati = cJSON_CreateArray();
+    cJSON *posti_prenotati = cJSON_CreateArray(); */
+
+    do{
+        int n_posti;
+        int tavolo;
+        printf("Che tavolo vuoi prenotare: ");
+        scanf("%d", &tavolo);
+        // controlliamo che il tavolo sia libero
+        cJSON *tav = cJSON_GetArrayItem(cJSON_GetObjectItem(sala, "tavoli"), tavolo - 1);
+        if(cJSON_IsTrue(cJSON_GetObjectItem(tav, "occupato"))){
+            printf("Il tavolo e' occupato\n");
+        } else {
+        // totale di posti del tavolo
+            printf("Il tavolo e' libero\n");
+            int posti_liberi = cJSON_GetObjectItem(tav, "posti_liberi")->valueint;
+            int posti_occupati = cJSON_GetObjectItem(tav, "posti_occupati")->valueint;
+            printf("Il tavolo ha %d posti liberi\n", posti_liberi);
+            printf("Quanti posti vuoi prenotare (0-%d): ", posti_liberi);
+            scanf("%d", &n_posti);
+            // controlliamo che ci siano abbastanza posti liberi
+            if(n_posti == posti_liberi){
+                // modifichiamo il tavolo
+                cJSON_ReplaceItemInObject(tav, "occupato", cJSON_CreateBool(1));
+            } else if (n_posti < posti_liberi){
+                cJSON_ReplaceItemInObject(tav, "posti_liberi", cJSON_CreateNumber(cJSON_GetObjectItem(tav, "posti_liberi")->valueint - n_posti));
+                cJSON_ReplaceItemInObject(tav, "posti_occupati", cJSON_CreateNumber(cJSON_GetObjectItem(tav, "posti_occupati")->valueint + n_posti));
+            } else {
+                printf("Non ci sono abbastanza posti liberi\n");
+            }
+
+            /* // salviamo i tavoli e i posti prenotati
+            cJSON_AddItemToArray(tavoli_prenotati, cJSON_CreateNumber(tavolo));
+            cJSON_AddItemToArray(posti_prenotati, cJSON_CreateNumber(n_posti)); */
+        }
+
+        // chiediamo se vuole prenotare un altro tavolo
+        char risposta[5];
+        printf("Vuoi prenotare un altro tavolo (S/N)?: ");
+        scanf("%s", risposta);
+        if(strcmp(risposta, "N") == 0 || strcmp(risposta, "n") == 0){
+            break;
+        }
+
+    } while(1);
+
+
+
+    // salviamo la sala con i tavoli prenotati nella cartella temporanea
+    char save_path[50];
+    sprintf(save_path, "%s/%s", path_temp, data);
+    salva_file_json(sala, save_path);
+
+    // stampiamo un messaggio di conferma
+    printf("Tavoli prenotati\n");
+
+    // creiamo un file json per la prenotazione
+    cJSON *prenotazione = cJSON_CreateObject();
+    cJSON_AddStringToObject(prenotazione, "username", username);
+    cJSON_AddStringToObject(prenotazione, "data", data);
+    cJSON_AddItemToObject(prenotazione, "sala", sala);
+    /* // inseriamo i tavoli e i posti prenotati nell'oggetto
+    cJSON_AddItemToObject(prenotazione, "tavoli", tavoli_prenotati);
+    cJSON_AddItemToObject(prenotazione, "posti", posti_prenotati); */
+    cJSON_AddBoolToObject(prenotazione, "confermata", 0);
+
+    // scriviamo il file temporaneo per il server
+    char save_path_server[50];
+    sprintf(save_path_server, "%s/%s", path_temp, "prenotazione.json");
+    salva_file_json(prenotazione, save_path_server);
+
+    // attendiamo la risposta del server con un ciclo che controlla se "confermata" è uguale a 0
+    cJSON *risposta = carica_file_json(save_path_server);
+    while(cJSON_IsFalse(cJSON_GetObjectItem(risposta, "confermata"))){
+        printf("In attesa di risposta\n");
+        risposta = carica_file_json(save_path_server);
+        Sleep(1000);
+    }
+    // eliminiamo il file prenotazione.json e la sala temporanea
+    remove(save_path_server);
+    remove(save_path);
+
+    // stampiamo la risposta del server
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    if (cJSON_IsTrue(cJSON_GetObjectItem(risposta, "confermata"))){
+        printf("%d-%02d-%02d %02d:%02d:%02d: Prenotazione confermata\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    } else {
+        printf("%d-%02d-%02d %02d:%02d:%02d: Prenotazione non confermata\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    }
+
+
+}
+
+
+// ############################################
 // FUNZIONI EFFETTIVE LATO SERVER
+// ############################################
 // funzione per stampare la guida
 void print_guida_server(){
     // apriamo il file txt in lettura
@@ -721,8 +881,23 @@ void approva_account(char *nome_file, char *path_account){
     salva_file_json(account, save_path);
 }
 
+// funzione per sloggare tutti gli utenti
+void logout_all(char *nome_file, char *path_account){
+
+    // carichiamo l'account
+    char save_path[50];
+    sprintf(save_path, "%s/%s", path_account, nome_file);
+    cJSON *account = carica_file_json(save_path);
+
+    // modifichiamo il valore del campo login in 0
+    cJSON_ReplaceItemInObject(account, "login", cJSON_CreateBool(0));
+
+    // salviamo l'account
+    salva_file_json(account, save_path);
+}
+
 // funzione per controllare gli account presenti nella cartella
-void controlla_account(char *path_account){
+void controlla_account(char *path_account, char *tipo_controllo){
 
     // creiamo un array di stringhe per contenere i nomi dei file presenti nella cartella
     char *files[100];
@@ -747,16 +922,31 @@ void controlla_account(char *path_account){
 
             // stampa il contenuto del file json
             // printf("%s\n", cJSON_Print(json));
-
-            cJSON *status = cJSON_GetObjectItemCaseSensitive(json, "status");   // prendiamo il campo status
-            // printf("Status: %d\n", cJSON_IsFalse(status));                   // stampiamo il valore del campo status per controllo
-            if(cJSON_IsFalse(status) == 1 && strcmp(files[count], ".") != 0 && strcmp(files[count], "..") != 0){
-                approva_account(files[count], path_account);
-                time_t t = time(NULL);
-                struct tm tm = *localtime(&t);
-                printf("%d-%02d-%02d %02d:%02d:%02d: Account %s approvato\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, files[count]);
+            // controlliamo che tipo di controllo dobbiamo fare, se tipo_controllo è uguale a "approva" allora approviamo l'account altrimenti rifiutiamo l'account
+            //printf("Tipo controllo: %s\n", tipo_controllo);
+            if (strcmp(tipo_controllo, "approva") == 0){
+                cJSON *status = cJSON_GetObjectItemCaseSensitive(json, "status");   // prendiamo il campo status
+                // printf("Status: %d\n", cJSON_IsFalse(status));                   // stampiamo il valore del campo status per controllo
+                if(cJSON_IsFalse(status) == 1 && strcmp(files[count], ".") != 0 && strcmp(files[count], "..") != 0){
+                    approva_account(files[count], path_account);
+                    time_t t = time(NULL);
+                    struct tm tm = *localtime(&t);
+                    printf("%d-%02d-%02d %02d:%02d:%02d: Account %s approvato\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, files[count]);
+                }
+                count++;
+            } else if (strcmp(tipo_controllo, "logout_all") == 0){
+                //printf("allert: %s\n", tipo_controllo);
+                cJSON *login = cJSON_GetObjectItemCaseSensitive(json, "login");   // prendiamo il campo status
+                if(cJSON_IsFalse(login) == 0 && strcmp(files[count], ".") != 0 && strcmp(files[count], "..") != 0){
+                    logout_all(files[count], path_account);
+                    time_t t = time(NULL);
+                    struct tm tm = *localtime(&t);
+                    printf("%d-%02d-%02d %02d:%02d:%02d: Account %s sloggato\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, files[count]);
+                }
+                count++;
+            } else {
+                printf("Tipo di controllo non riconosciuto\n");
             }
-            count++;
         }
         closedir(dr);
     }
@@ -840,4 +1030,69 @@ void logout_check(char *temp_path, char *path){
     }
 }
 
+// funzione per prenotare un tavolo
+void conferma_prenotazione(char *temp_path, char *path_sala){
+
+    // carichiamo il file prenotazione.json
+    char save_path[50];
+    sprintf(save_path, "%s/prenotazione.json", temp_path);
+    cJSON *prenotazione = carica_file_json(save_path);
+
+    // estraiamo la stringa username dal file json
+    cJSON *username = cJSON_GetObjectItemCaseSensitive(prenotazione, "username");
+
+    // estraiamo la data dal file json
+    cJSON *data = cJSON_GetObjectItemCaseSensitive(prenotazione, "data");
+
+    // estraiamo il cJSON sala dal file prenotazione
+    cJSON *sala = cJSON_GetObjectItem(prenotazione, "sala");
+
+    // salviamo la sala con i tavoli prenotati nella cartella sala
+    char save_path_sala[50];
+    sprintf(save_path_sala, "%s/%s.json", path_sala, data->valuestring);
+    salva_file_json(sala, save_path_sala);
+
+    // creiamo un controllo per vedere se il file esiste
+    int codice = 0;
+    char path_prenotazione[50];
+    sprintf(path_prenotazione, "%s/prenotazioni.csv", path_sala);
+    if(!se_esiste(path_sala, "prenotazioni", "csv")){
+        // se il file non esiste allora creiamo il file e scriviamo l'intestazione
+        //printf("File non esistente\n");
+        FILE* file = fopen(path_prenotazione, "w");
+        //printf("File creato\n");
+        //fprintf(file, "codide,username,data\n");
+        fprintf(file, "%d,%s,%s\n", codice, username->valuestring, data->valuestring);
+        //printf("Prenotazione scritta\n");
+        fclose(file);
+
+    } else {
+        // se il file esiste allora controlliamo l'ultimo codice
+        //printf("File esistente\n");
+        FILE* file = fopen(path_prenotazione, "a");
+        char riga[MAX_LUNG_PORTATA];
+        while(fgets(riga, MAX_PORTATE, file) != NULL){
+            codice++;
+        }
+        //printf("Ultimo codice: %d\n", codice);
+        fprintf(file, "%d,%s,%s\n", codice, username->valuestring, data->valuestring);
+        //printf("Prenotazione scritta\n");
+        fclose(file);
+    }
+
+    // modifichiamo il valore del campo confermata in 1
+    cJSON_ReplaceItemInObject(prenotazione, "confermata", cJSON_CreateBool(1));
+    //printf("Valore di prenotazione in confermata\n");
+    cJSON *confermata = cJSON_GetObjectItem(prenotazione, "confermata");
+    //printf("Confermata: %d\n", cJSON_IsTrue(confermata));
+
+    // salviamo il file json della prenotazione
+    salva_file_json(prenotazione, save_path);
+
+    // stampiamo un messaggio di conferma
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    printf("%d-%02d-%02d %02d:%02d:%02d: Prenotazione confermata\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);    
+
+}
             
